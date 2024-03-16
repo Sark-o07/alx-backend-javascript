bda033f6ef196d4d9078e07dfeb3347a3f0f2039
Sark-o07 <kosinick01@gmail.com>
2024-03-16 17:28:07 +0000

    Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,21 +1,21 @@
 /**
- * a weak map of endpoints and number of calls made.
+ * A weak map of endpoints and the number of calls made.
  */
 export const weakMap = new WeakMap();
 
 /**
- * the maximum number of calls for an endpoints.
+ * The maximum number of calls for an endpoint.
  */
 const MAX_ENDPOINT_CALLS = 5;
 
 /**
- * Tracks the number of call made to an API's endpoint.
+ * Tracks the number of calls made to an API's endpoint.
  * @param {{
  *   protocol: String,
  *   name: String,
- * }} - endpoint -  - The endpoint to make a request to.
+ * }} endpoint - The endpoint to make a request to.
  */
-export default function queryAPI(endpoint) {
+export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
